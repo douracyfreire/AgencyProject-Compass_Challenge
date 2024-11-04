cad32294879a15f9f86286c14aea8d93ae083fee
Douracy Freire <mdouracy.fc@gmail.com>
2024-11-04 12:06:34 -0300

    Creating modal for the best opportunities section

--- src/js/home.js
+++ src/js/home.js
@@ -1,57 +1,87 @@
-function loadData(){
-  fetch("/data.json")
-    .then(response => response.json())
-    .then(data => {
-      const container = document.querySelector("#container-better-opportunities");
+fetch("/data.json")
+  .then((response) => response.json())
+  .then((data) => {
+    const container = document.querySelector("#container-better-opportunities");
 
-      data.map(opportunity => {
-        const card = document.createElement("div");
-        card.classList.add("card-opportunity");
+    data.map((opportunity) => {
+      const card = document.createElement("div");
+      card.classList.add("card-opportunity");
 
-        const img = document.createElement("div");
-        img.classList.add("image-opportunity");
-        const imageOpportunity = opportunity.image;
-        img.style.backgroundImage = `url("${imageOpportunity}")`;
+      const img = document.createElement("div");
+      img.classList.add("image-opportunity");
+      const imageOpportunity = opportunity.image;
+      img.style.backgroundImage = `url("${imageOpportunity}")`;
 
-        const price = document.createElement("h3");
-        price.textContent = opportunity.price;
+      const price = document.createElement("h3");
+      price.textContent = opportunity.price;
 
-        const size = document.createElement("p");
-        const area = document.createElement("span");
-        area.textContent = opportunity.size;
-        size.appendChild(area);
-        const textMeasure = document.createTextNode(" m²");
-        size.appendChild(textMeasure);
-        
-        const listRooms = document.createElement("ul");
-        listRooms.classList.add("tags-rooms");
+      const size = document.createElement("p");
+      const area = document.createElement("span");
+      area.textContent = opportunity.size;
+      size.appendChild(area);
+      const textMeasure = document.createTextNode(" m²");
+      size.appendChild(textMeasure);
 
-        const elementBedroom = document.createElement("li");
-        const bedrooms = document.createElement("span");
-        bedrooms.textContent = opportunity.bedrooms;
-        elementBedroom.appendChild(bedrooms);
-        const textBedroom = document.createTextNode(" quartos");
-        elementBedroom.appendChild(textBedroom);
+      const listRooms = document.createElement("ul");
+      listRooms.classList.add("tags-rooms");
 
-        listRooms.appendChild(elementBedroom);
+      const elementBedroom = document.createElement("li");
+      const bedrooms = document.createElement("span");
+      bedrooms.textContent = opportunity.bedrooms;
+      elementBedroom.appendChild(bedrooms);
+      const textBedroom = document.createTextNode(" quartos");
+      elementBedroom.appendChild(textBedroom);
 
-        const elementBathroom = document.createElement("li");
-        const bathrooms = document.createElement("span");
-        bathrooms.textContent = opportunity.bathrooms;
-        elementBathroom.appendChild(bathrooms);
-        const textBathroom = document.createTextNode(" banheiros");
-        elementBathroom.appendChild(textBathroom);
+      listRooms.appendChild(elementBedroom);
 
-        listRooms.appendChild(elementBathroom);
+      const elementBathroom = document.createElement("li");
+      const bathrooms = document.createElement("span");
+      bathrooms.textContent = opportunity.bathrooms;
+      elementBathroom.appendChild(bathrooms);
+      const textBathroom = document.createTextNode(" banheiros");
+      elementBathroom.appendChild(textBathroom);
 
-        card.appendChild(img);
-        card.appendChild(price);
-        card.appendChild(size);
-        card.appendChild(listRooms);
+      listRooms.appendChild(elementBathroom);
 
-        container.appendChild(card);
+      card.appendChild(img);
+      card.appendChild(price);
+      card.appendChild(size);
+      card.appendChild(listRooms);
+
+      container.appendChild(card);
+      
+      card.addEventListener('click', () => {
+        openModal(opportunity);
       });
     });
+  });
+
+const modal = document.getElementById("modal");
+
+function openModal(cardElement){
+  const modal = document.getElementById("modal");
+  const modalImage = document.getElementById("image-modal");
+  const modalPrice = document.getElementById("price-modal");
+  const modalBedrooms = document.getElementById("bedrooms-modal");
+  const modalBathrooms = document.getElementById("bathrooms-modal");
+  const modalSize = document.getElementById("size-modal");
+        
+  const imageCardElement = cardElement.image;
+  modalImage.style.backgroundImage = `url("${imageCardElement}")`;
+  modalPrice.textContent = cardElement.price;
+  modalBedrooms.textContent = cardElement.bedrooms;
+  modalBathrooms.textContent = cardElement.bathrooms;
+  modalSize.textContent = cardElement.size;
+
+  modal.showModal();
 }
 
-loadData();
+document.addEventListener("click", (event) => {
+  if(event.target === modal){
+    modal.close();
+  }
+});
+
+document.querySelector(".close").addEventListener("click", () => {
+  modal.close();
+});
